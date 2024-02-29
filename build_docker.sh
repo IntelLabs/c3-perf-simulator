29443bbb4fc00c3900908d7f1e0ea0e8206fcc1e
Yash Jain <y1jain@ucsd.edu>
2024-02-29 07:09:17 -0800

    Cleanup README and docker build script

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,8 @@
 DOCKER_NAME=c3-gem5-docker
-GEM5_SRC_DIR=./c3-perf-simulator
 
 mkdir outputs
 docker build -t $DOCKER_NAME .
+# Run docker
 #docker run -u 0 -it $DOCKER_NAME
+# Run docker with outputs directory mounted (to retrieve any test results)
 docker run -u 0 --volume ./outputs:/outputs -it $DOCKER_NAME
