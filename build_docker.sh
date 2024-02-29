84b354921dfe4d8a0450c5143fded5a34b7e14f1
Yash Jain <y1jain@ucsd.edu>
2024-02-29 12:42:52 -0800

    Update docker build script & .gitignore

--- build_docker.sh
+++ build_docker.sh
@@ -3,5 +3,5 @@ GEM5_SRC_DIR=./c3-perf-simulator
 
 mkdir outputs
 docker build -t $DOCKER_NAME .
-docker run -u 0 -it $DOCKER_NAME
-#docker run -u 0 --volume ./outputs:/outputs -it $DOCKER_NAME
+#docker run -u 0 -it $DOCKER_NAME
+docker run -u 0 --volume ./outputs:/outputs -it $DOCKER_NAME
